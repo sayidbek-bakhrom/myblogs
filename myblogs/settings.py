81c1d83c12d5ee00c7e818183f8ebde713442ee7
Sayidbek <sayidbekbakhrom@gmail.com>
2022-08-25 21:38:41 +0500

    tweaked app for heroku

--- myblogs/settings.py
+++ myblogs/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-v)tfp1qq_tazfh@dy+qmhc0+)=&s_z69pl6hn&mnt4#rk!jg9^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['https://myblogs2022.herokuapp.com/']
 
 
 # Application definition
