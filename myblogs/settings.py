83b0b18f9040e93e0cb27292730d3e079ca68fc0
Sayidbek <sayidbekbakhrom@gmail.com>
2022-08-25 21:01:29 +0500

    tweaked app for heroku

--- myblogs/settings.py
+++ myblogs/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v)tfp1qq_tazfh@dy+qmhc0+)=&s_z69pl6hn&mnt4#rk!jg9^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
