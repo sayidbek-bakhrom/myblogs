c8d5abd87f9e5d8ddd53e51532e202dd1ca03b1c
Sayidbek <sayidbekbakhrom@gmail.com>
2022-08-25 21:22:23 +0500

    tweaked app for heroku

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,12 @@ from .forms import PostForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 
-# Pagination imports
-from django.core.paginator import Paginator
-
 
 def home(request):
     # p = Paginator(Post.objects.all(), 10)
     # page = request.GET.get('page')
     posts = Post.objects.all().order_by('updated_at')
+
     # posts = p.get_page(page)
     # nums = 'a' * posts.paginator.num_pages
     context = {
