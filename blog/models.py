9a1f8c4b58a2faf2be8edd9812282cbaf9c434e1
Sayidbek <sayidbekbakhrom@gmail.com>
2022-08-26 10:11:40 +0500

    debug true

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to='', blank=True)
     content = RichTextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
